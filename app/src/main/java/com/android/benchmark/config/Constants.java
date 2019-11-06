e6deb4a43128fcce3fe033fb58717f64295767d1
Josh Choo <dev@joshuous.com>
2019-11-07 01:57:23 +0800

    refactor: create class to store constants (e.g. endpoint url, app version)

---
+++ app/src/main/java/com/android/benchmark/config/Constants.java
@@ -0,0 +1,8 @@
+package com.android.benchmark.config;
+
+public class Constants {
+    private Constants() {}
+
+    public static final String BASE_URL = "https://jankbench.herokuapp.com/";
+    public static final String BENCHMARK_VERSION = "0.1";
+}
